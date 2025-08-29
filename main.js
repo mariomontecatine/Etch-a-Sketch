7f4d63054f68b9207cd4c0b029a1a6f8a9930577
mariomontecatine <170352659+mariomontecatine@users.noreply.github.com>
2025-08-29 18:19:36 +0200

    Hover  effect finished

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const container = document.querySelector("#container");
-
-for (let i = 0; i < (16 * 16); i++) {
+let gridSize = 16;
+// Iterating to create the 16x16 divs grid
+for (let i = 0; i < (gridSize * gridSize); i++) {
     const containerElement = document.createElement("div");
     containerElement.textContent = `${i}`;
     containerElement.classList.add("containerElement");
+    containerElement.addEventListener("mouseover", () => {
+        containerElement.setAttribute("style", "background-color: beige;");
+    }, false);
+
     container.appendChild(containerElement);
 }
+
 
