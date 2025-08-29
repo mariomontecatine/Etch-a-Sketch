8bd93409e10fa6a25ece3b95a81a95e0c5f2c14b
mariomontecatine <170352659+mariomontecatine@users.noreply.github.com>
2025-08-29 19:10:24 +0200

    Project finished, except for adapting grid size
     Changes to be committed:
    	modified:   index.html
    	modified:   main.js
    	modified:   style.css

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 const container = document.querySelector("#container");
 let gridSize = 16;
-// Iterating to create the 16x16 divs grid
-for (let i = 0; i < (gridSize * gridSize); i++) {
-    const containerElement = document.createElement("div");
-    containerElement.textContent = `${i}`;
-    containerElement.classList.add("containerElement");
-    containerElement.addEventListener("mouseover", () => {
-        containerElement.setAttribute("style", "background-color: beige;");
-    }, false);
+displayGrid();
 
-    container.appendChild(containerElement);
+// Iterating to create the gridSizeXgridSize divs grid
+function displayGrid() {
+    container.innerHTML = "";
+
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        const containerElement = document.createElement("div");
+        containerElement.classList.add("containerElement");
+        containerElement.addEventListener("mouseover", () => {
+            containerElement.setAttribute("style", "background-color: beige;");
+        }, false);
+
+        container.appendChild(containerElement);
+    }
 }
+const controls = document.querySelector("#controls");
+const changeGridSizeButton = document.createElement("button");
+changeGridSizeButton.textContent = "Change size";
+changeGridSizeButton.classList.add("changeGridSizeButton");
+
+controls.appendChild(changeGridSizeButton);
 
 
+
+changeGridSizeButton.addEventListener("click", () => {
+    const sizeChosePrompt = window.prompt("Enter the size that you want (<=100)");
+    gridSize = sizeChosePrompt;
+    displayGrid();
+}, false);
