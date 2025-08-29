e8ac57c7296472d3f4157c4e3f86caaf0c56cad4
mariomontecatine <170352659+mariomontecatine@users.noreply.github.com>
2025-08-30 01:19:34 +0200

    Flexbox correctly fixed

--- main.js
+++ main.js
@@ -1,21 +1,32 @@
 const container = document.querySelector("#container");
-let gridSize = 16;
+let gridSize = 6;
 displayGrid();
 
 // Iterating to create the gridSizeXgridSize divs grid
 function displayGrid() {
     container.innerHTML = "";
 
-    for (let i = 0; i < (gridSize * gridSize); i++) {
+    for (let i = 0; i < gridSize; i++) {
         const containerElement = document.createElement("div");
         containerElement.classList.add("containerElement");
-        containerElement.addEventListener("mouseover", () => {
-            containerElement.setAttribute("style", "background-color: beige;");
-        }, false);
-
         container.appendChild(containerElement);
+        for (let j = 0; j < gridSize; j++) {
+            const grid = document.createElement('div');
+            grid.setAttribute('class', 'grid');
+            grid.style.opacity = 0;
+            containerElement.appendChild(grid);
+        }
+
+
     }
+    const grids = document.querySelectorAll('.grid');
+    grids.forEach((element) => {
+        element.addEventListener("mouseover", () => {
+            element.setAttribute("style", "background-color: beige;");
+        }, false);
+    })
 }
+
 const controls = document.querySelector("#controls");
 const changeGridSizeButton = document.createElement("button");
 changeGridSizeButton.textContent = "Change size";
